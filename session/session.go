14e20fa79c2425c4205cfeda5fe8f019b61a7237
FDUTCH <para.trabajos9999@gmail.com>
2024-09-22 04:59:50 +0200

    finally fixed?

--- session/session.go
+++ session/session.go
@@ -361,6 +361,7 @@ func (s *Session) clearScoreboard() {
 }
 
 func (s *Session) changeDimension(dimension int32, pos mgl32.Vec3) {
+	s.currentDimension.Store(dimension)
 	_ = s.conn.WritePacket(&packet.ChangeDimension{
 		Dimension: dimension,
 		Position:  pos,
