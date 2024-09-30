0c3cea30de81bde9574cc27e843ce5f6716e8ad9
Alexander <123891354+FDUTCH@users.noreply.github.com>
2024-09-30 23:21:52 +0200

    Update packet_handler.go

--- session/packet_handler.go
+++ session/packet_handler.go
@@ -66,7 +66,7 @@ func handlePackets(s *Session) {
 						_ = s.conn.WritePacket(&packet.SetDifficulty{Difficulty: uint32(gameData.Difficulty)})
 						_ = s.conn.WritePacket(&packet.GameRulesChanged{GameRules: gameData.GameRules})
 						_ = s.conn.WritePacket(&packet.SetPlayerGameType{GameType: gameData.PlayerGameMode})
-						_ = s.clientConn.WritePacket(&packet.SetTime{Time: int32(serverGameData.Time)})
+						_ = s.conn.WritePacket(&packet.SetTime{Time: int32(gameData.Time)})
 						
 						w.Wait()
 
